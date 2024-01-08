ba5b24b7639a08538e3894cf965a1edeccf17e74
cloudali <108419678+cloudali@users.noreply.github.com>
2024-01-07 20:38:01 -0600

    Updating visitor counter

--- website/assets/js/views.js
+++ website/assets/js/views.js
@@ -4,7 +4,7 @@
 
     async function updateCounter() {
         const counter = document.getElementById("visitor");
-          let response = await fetch("https://ivtom7z4ocfndyvs5f67dy2d7a0zsmrt.lambda-url.us-east-1.on.aws/");
+          let response = await fetch("https://2ssl447l52qicn3paqeqj4g6km0kydsl.lambda-url.us-east-1.on.aws/");
           let data = await response.json();
           console.log(data);
           counter.innerHTML = `${data}`;
